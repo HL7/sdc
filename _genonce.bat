d355f3330b41794ffccac63841625506b964e825
Lloyd McKenzie <lmckenzie@gevityinc.com>
2019-09-05 11:45:45 -0600

    Updated to new publishing framework

--- _genonce.bat
+++ _genonce.bat
@@ -1,3 +1,4 @@
-SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8 -DupdatePublisher=N
-java -jar framework\ant\ant-launcher.jar -buildfile framework/build.xml
-pause
+@SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+REM XCOPY /M/E/Q/Y _template C:\Users\lloyd\.fhir\packages\fhir.test.template#current
+JAVA -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini
+@PAUSE
