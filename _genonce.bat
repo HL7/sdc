c8a4eebe0776f47d37ce46aea58cefc8843bc0a9
Lloyd McKenzie <lmckenzie@gevityinc.com>
2019-11-12 12:05:09 -0700

    Updates to bat and removed properties.  Don't need to treat CapabilityStatements as examples anymore

--- _genonce.bat
+++ _genonce.bat
@@ -1,4 +1,3 @@
 @SET JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
-REM XCOPY /M/E/Q/Y _template C:\Users\lloyd\.fhir\packages\fhir.test.template#current
 JAVA -jar input-cache/org.hl7.fhir.publisher.jar -ig ig.ini
 @PAUSE
